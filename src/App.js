a9f7bfbc8605e0d537c58020de69cdd57b4c8091
Gabriel Guesser <gabrieldguesser@hotmail.com>
2020-07-14 01:53:04 -0300

    showing products from the api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import Header from './components/Header/index'
+import Header from './components/Header/index';
+import Api from './services/api'
+import Main from './pages/main'
+
+import './styles.css';
+
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 
